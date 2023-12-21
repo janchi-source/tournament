c0cf353ceda0e8778f911a2c43f5e246772a2167
ajem-pito <>
2023-12-21 10:39:20 +0100

    added render logic

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame as pg
-import tilesmanger
+import tilesmanger as tmg
 
 # setting up numbers
 pg.init()
@@ -18,6 +18,7 @@ max_w = (pg.display.get_surface().get_width() - margin) // (tile_width+margin)
 max_h = (pg.display.get_surface().get_height() - margin) // (tile_heigth+margin)
 win.fill(GRAY)
 clock = pg.time.Clock()
+tm = tmg.tilesmanger(win)
 
 def gen_cells():
     for col in range(max_w):
@@ -28,11 +29,16 @@ def gen_cells():
             rect = pg.Rect(x, y, tile_width, tile_heigth)
             pg.draw.rect(win, BLACK, rect)
             y += tile_heigth + margin
-            cells[col].append(rect)
+            cells[col].append(tmg.tile(tm, rect, tmg.Direction.NONE))
         x += tile_width + margin
 
 def main():
     gen_cells()
+    cells[5][5].side = (tmg.Direction.UP,tmg.Direction.DOWN,tmg.Direction.LEFT,tmg.Direction.RIGHT)
+    for row in cells:
+        for tile in row:
+            tile.handle_side()
+
     running = True
     while running:
         for e in pg.event.get(): 
@@ -41,9 +47,9 @@ def main():
         if e.type == pg.MOUSEBUTTONUP and e.button == 1: # on mouse click
             x,y = e.pos
             for row in cells:
-                for rect in row:
-                    if rect.collidepoint(x,y):
-                        pg.draw.rect(win, WHITE, rect)
+                for tile in row:
+                    if tile.rect.collidepoint(x,y):
+                        pg.draw.rect(win, WHITE, tile.rect)
         pg.display.update()
 
     pg.quit()
--- tilesmanger.py
+++ tilesmanger.py
@@ -1,4 +1,62 @@
+from enum import Enum
+import pygame as pg
+
+Direction = Enum("Direction",["UP", "DOWN", "LEFT", "RIGHT", "NONE"])
+
+YELLOW = (237,219,90)
+BLACK = (0,0,0)
+
 class tilesmanger:
-    def __init__(self) -> None:
-        tileset = [] # (down, right, up, left)
-        
+    def __init__(self, win: pg.Surface) -> None:
+        self.win = win
+        # self.tilemap = [
+        #     tile(Direction.UP),tile(Direction.DOWN),tile(Direction.RIGHT),tile(Direction.LEFT)
+        # ]
+
+class tile:
+    def __init__(self, tm: tilesmanger, rect: pg.Rect, side: Direction = Direction.NONE) -> None:
+        self.side = side
+        self.rect = rect
+        self.win = tm.win
+        self.collider = None
+
+    def handle_side(self):
+        self.handle_none()
+        if type(self.side) != tuple:
+            self.render_side(self.side)
+            return
+        for side in self.side:
+            self.render_side(side)
+    
+    def render_side(self, side):
+        match side:
+            case Direction.UP: self.handle_up()
+            case Direction.DOWN: self.handle_down()
+            case Direction.LEFT: self.handle_left()
+            case Direction.RIGHT: self.handle_right()
+            case Direction.NONE: self.handle_none()
+            case _: raise ValueError("?!")
+    
+    def handle_none(self):
+        self._set_colour(YELLOW)
+    
+    def handle_up(self):
+        self.collider = pg.Rect(self.rect.left, self.rect.top, self.rect.width, 5)
+        pg.draw.rect(self.win, BLACK, self.collider)
+
+    def handle_down(self):
+        self.collider = pg.Rect(self.rect.left, self.rect.bottom-5, self.rect.width, 5)
+        pg.draw.rect(self.win, BLACK, self.collider)
+
+    def handle_left(self):
+        self.collider = pg.Rect(self.rect.left, self.rect.top, 5, self.rect.height)
+        pg.draw.rect(self.win, BLACK, self.collider)
+        print(f"{self.rect.left}, {self.rect.bottom}, 5,{self.rect.height}")
+
+    def handle_right(self):
+        self.collider = pg.Rect(self.rect.right-5, self.rect.top, 5, self.rect.height)
+        pg.draw.rect(self.win, BLACK, self.collider)
+        print(f"{self.rect.left}, {self.rect.bottom}, 5,{self.rect.height}")
+    
+    def _set_colour(self, colour):
+        pg.draw.rect(self.win, colour, self.rect)
